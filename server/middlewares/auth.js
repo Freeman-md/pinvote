633c296593e58b5f4f67c4a0de010692599df252
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2023-11-20 02:16:37 +0000

    UPDATE: implement request password reset functionality without emailing
    create unique token on every request and generate password reset link

--- server/middlewares/auth.js
+++
@@ -1,7 +0,0 @@
-export const isAuth = (req, res, next) => {
-    if (!req.session.authenticated && !req.session.user) {
-        return res.redirect('/auth/login')
-    }
-
-    next()
-}
--- server/utils/helpers.js
+++
@@ -1,5 +0,0 @@
-export const flashErrorsAndRedirect = (req, res, validationResult) => {
-    req.flash('errors', validationResult.errors);
-    req.flash('formData', validationResult.formData);
-    res.redirect('back');
-};
