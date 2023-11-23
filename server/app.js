a95d8a952662df72e541bbf28259fb42b936f7f7
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2023-11-23 19:57:46 +0000

    UPDATE: update app.js

--- server/app.js
+++ server/app.js
@@ -28,7 +28,7 @@ const sessionStore = new MongoDBStore(session)({
 const csrfProtection = csrf()
 
 // view engine setup
-app.set('views', path.join(__dirname, '../public/views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
