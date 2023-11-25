07fcadf2e1bf28fdbba1339230bc8c443be15766
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2023-11-25 16:45:52 +0000

    UPDATE: delete password reset mail job once complete

--- server/jobs/scheduler.js
+++ server/jobs/scheduler.js
@@ -3,7 +3,9 @@ import Events from "../lib/emitter/events";
 
 const scheduler = {
   sendPasswordResetMail: async (data) => {
-    await agenda.now(Events.SEND_PASSWORD_RESET_MAIL, data);
+    const job = await agenda.now(Events.SEND_PASSWORD_RESET_MAIL, data);
+
+    await job.remove()
   },
 
 }
